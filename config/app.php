3826f6eb557459e105f8a8b71e0184f2eafac280
giangann <gta007md@gmail.com>
2021-09-04 07:52:06 +0700

    Install Socialite
    add providers and aliases in config file

--- config/app.php
+++ config/app.php
@@ -230,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'providers' => Laravel\Socialite\SocialiteServiceProvider::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
